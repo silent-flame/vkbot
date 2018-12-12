eed000cb3354247b58679afdcc060b94f3463680
silent-flame <bormotan1@yandex.ru>
2018-12-12 03:53:35 +0300

    get user

--- src/main/java/silentflame/conf/VkBot.java
+++ src/main/java/silentflame/conf/VkBot.java
@@ -3,6 +3,7 @@ package silentflame.conf;
 import com.petersamokhin.bots.sdk.clients.Group;
 import com.petersamokhin.bots.sdk.objects.Message;
 import lombok.extern.slf4j.Slf4j;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -27,8 +28,25 @@ public class VkBot {
                     .to(message.authorId())
                     .text("Hello")
                     .send();
+            getUser(message.authorId());
             System.out.println(message);
             log.info("Retrieved message {}", message);
         });
+    }
+
+    public void sendMessage(Integer userId, String text) {
+        new Message()
+                .from(group)
+                .to(userId)
+                .text(text)
+                .send();
+    }
+
+    public void getUser(Integer userId) {
+        JSONObject params = new JSONObject();
+        params.put("user_ids", userId);
+        params.put("name_case", "nom");
+        group.api().call("users.get", params, response -> log.info("Callback response {}",response));
+
     }
 }
